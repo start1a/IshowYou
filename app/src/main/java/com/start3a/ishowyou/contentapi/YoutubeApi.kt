847692faf083881cbca157a07b6ad642d9f13f28
start3a <start3a@gmail.com>
2021-03-27 16:10:50 +0900

    fix get videos function in youtubeApi.kt

--- app/src/main/java/com/start3a/ishowyou/contentapi/YoutubeApi.kt
+++ app/src/main/java/com/start3a/ishowyou/contentapi/YoutubeApi.kt
@@ -25,7 +25,6 @@ interface YoutubeApi {
         @Query("q") q: String,
         @Query("key") key: String = ApiKey.key,
         @Query("type") type: String = "video",
-        @Query("eventType") eventType: String = "completed",
         @Query("part") part: String = "snippet",
         @Query("maxResults") maxResult: Int = 50
     ): Call<YoutubeSearchJsonData>
